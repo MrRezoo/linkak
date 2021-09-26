bc703bb1b7f152dba9c315f8b227cef87d4b1c00
Mr.Rezoo <rezam578@gmail.com>
2021-09-26 16:51:34 +0330

    Chore(accounts/models): add db_table for user model

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -40,6 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+        db_table = 'user'
 
     def clean(self):
         super().clean()
